12fbcfda05b2f825fd4f0b0515c2caa1315f2d1f
Aviad Levy <aviadlevy1@gmail.com>
2016-08-30 17:30:00 +0300

    change logfile name add to .gitignore

--- ktuvitDownloader/const.py
+++ ktuvitDownloader/const.py
@@ -15,7 +15,7 @@ SUB_EXT = [".srt", ".sub"]
 VIDEO_EXT = [".webm", ".mkv", ".flv", ".avi", ".mov", ".wmv", ".rm", ".rmvb", ".mp4", ".m4p", ".m4v", ".mpg", ".mpeg",
              "mp2", ".mpe", ".mpv", ".m2v", ".m4v"]
 
-CONFIG_FILE = ".config.cfg"
+CONFIG_FILE = ".ktuvitConfig.cfg"
 LOG_FILE = ".ktuvitLogger.log"
 
 MB = 1024 * 1024
